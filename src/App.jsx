23783e9a5f8bb3bd8642979b6af0802b9b25fcf0
Anish Kumar <anishkumar6527@gmail.com>
2024-01-24 00:07:35 +0530

    creating ui

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,44 @@ function App() {
 
   return (
     <>
-      
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-8 my-8 text-orange-500 bg-gray-800">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className = "flex shadow rounded-lg overflow-hidden mb-4">
+          <input 
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="Password"
+          readOnly/>
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">copy</button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input type="range" min={6} max={24} value={length} className="cursor-pointer" onChange={(e) => {setLength(e.target.value)}} />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+          <input 
+            type="checkbox"
+            defaultChecked={numAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNumAllowed((prev) => !prev);
+            }}/>
+             <label>Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+          <input 
+            type="checkbox"
+            defaultChecked={numAllowed}
+            id="numberInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}/>
+             <label>Characters</label>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
